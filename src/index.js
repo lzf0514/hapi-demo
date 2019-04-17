f861af7c700c9084d624ea7fb71e078b42408188
Lucas Lai <llai@xogrp.com>
2019-04-17 16:43:49 +0800

    authentication

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 const Hapi = require('hapi');
+const Bcrypt = require('bcrypt');
+
+const user = {
+  username: 'john',
+  password: '$2b$10$8YOSrxT75t4Okr4R6W5NzemF/Gj1XkEznZXyJlfLY/dzcdWcgtETy',
+  name: 'John Doe',
+  id: '23512545'
+}
+
+const validate = async (request, username, password) => {
+  if (username !== user.username) {
+    return { credentials: null, isValid: false }
+  }
+
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, name: user.name };
+
+  return { isValid, credentials };
+}
 
 const init = async () => {
   const server = Hapi.server({
@@ -6,13 +25,18 @@ const init = async () => {
     host: 'localhost'
   })
 
+  await server.register(require('hapi-auth-basic'));
+  server.auth.strategy('myAuth', 'basic', { validate });
   await server.register(require('inert'));
 
   server.route({
     method: 'GET',
     path: '/',
+    options: {
+      auth: 'myAuth'
+    },
     handler: (request, h) => {
-      return 'Hello World';
+      return `Hello ${request.auth.credentials.name}`;
     }
   });
 
