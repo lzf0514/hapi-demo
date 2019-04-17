5eec04f7c0e21db0054cb37650065aa14a24302e
Lucas Lai <llai@xogrp.com>
2019-04-17 12:00:29 +0800

    hapi routers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ const init = async () => {
     host: 'localhost'
   })
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World';
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+      return `Hello ${encodeURIComponent(request.params.name)}!`;
+    }
+  })
+
   await server.start();
   console.log('Server running on %ss', server.info.uri);
 }
